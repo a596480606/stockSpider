c661833c037b0d1d289e93bd6a4ceb4dbb7b5627
朱程辉 <5381006+zhu_cheng_hui@user.noreply.gitee.com>
2020-03-14 13:36:50 +0800

    数据库配置文件

---
+++ SalveSpider/settings.py
@@ -0,0 +1,19 @@
+"""
+# -*- coding: utf-8 -*-
+"""
+# redis 数据库配置
+RedisHost = "127.0.0.1"
+RedisPort = "6379"
+RedisPassword = "123456"
+RedisDB = "6"
+
+# mysql 数据库配置
+MysqlHost = "127.0.0.1"
+MysqlPort = 3306
+MysqlUser = "root"
+MysqlPassword = "123456"
+MysqlDB = "Stock"
+
+
+
+
